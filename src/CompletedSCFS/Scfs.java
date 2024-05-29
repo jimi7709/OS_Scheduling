c2c56cf66b00270c421c4d424c3726bd0edb126e
Jaemin Park <qkrwoals8697@yu.ac.kr>
2024-05-30 00:13:18 +0900

    IntegratedPriorityNonemptive

--- src/CompletedSCFS/Scfs.java
+++ src/CompletedSCFS/Scfs.java
@@ -20,14 +20,14 @@ class Scfs {
 			for (int i = 0; i < jobList.size(); i++) {
 				if (jobList.get(i).arriveTime <= runTime) {// 프로세스가 도착해서 실행가능하면
 					runTime += jobList.get(i).burstTime;// burstTime 만큼 runtime 증시킨다.(프로세스 실행됨.)
-					if(i<jobList.size()-1)
+					if(i<jobList.size()-1)//다음 프로세스가 존재한다면
 						if(jobList.get(i+1).arriveTime<= runTime)//현재 프로세스가 실행됐고, 다음 프로세스가 미리 와서 기다리고 있다면
 							jobList.get(i+1).waitingTime += (runTime - jobList.get(i+1).arriveTime);
 				}
 				else {//arrriveTime> runTime 아직 프로세스가 도착하지 않아서 실행불가능하면
 					runTime += jobList.get(i).arriveTime; //프로세스 도착시간만큼의 시간이 흐르고
 					runTime += jobList.get(i).burstTime;// burstTime 만큼 runtime 증시킨다.(프로세스 실행됨.)
-					if(i<jobList.size()-1)
+					if(i<jobList.size()-1)//다음 프로세스가 존재한다면
 						if(jobList.get(i+1).arriveTime<= runTime)//현재 프로세스가 실행됐고, 다음 프로세스가 미리 와서 기다리고 있다면
 							jobList.get(i+1).waitingTime += (runTime - jobList.get(i+1).arriveTime);
 				}
